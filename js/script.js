bdf02ecbf1551f6655f8fca1e5e19c5f55fa9c4b
Emily Johnson <emelizajo22@gmail.com>
2022-06-10 12:31:13 -0700

    change css keyframes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 //onclick events
 
+//sign up newsletter button
+$("#newsDropBtn").on('click', function() {
+  $(this).show();
+  $("#signUp-content").show();
+  $(".pyrateHeader").hide();
+});
+
+$("#newsImg").on('click', function() {
+  $(this).show();
+  $("#signUp-content").hide();
+  $(".pyrateHeader").show();
+});
+
 //rules and directions button
 $("#rulesDropBtn").on('click', function() {
   $(this).show();
-  $(".dropDown-content").show();
+  $("#rulesDirections-content").show();
   $(".pyrateHeader").hide();
 });
 
 $("#rulesImg").on('click', function() {
   $(this).show();
-  $(".dropDown-content").hide();
+  $("#rulesDirections-content").hide();
   $(".pyrateHeader").show();
 });
 
@@ -30,6 +43,118 @@ $("#exitGame").on('click', function() {
 });
 
 
+//water movement
+
+let h = $('.flag').width();
+for(let i = 0; i < h; i++){
+    let flagElement = $("<div class='flag-element'>");
+    flagElement.css('background-position', -i + "px 0");
+    flagElement.css('-webkit-animation-delay', i * 2 + 'ms');
+    flagElement.css('-moz-animation-delay', i * 2 + 'ms');
+    flagElement.css('-ms-animation-delay', i * 2 + 'ms');
+    flagElement.css('animation-delay', i * 2 + 'ms');
+    $('.flag').append(flagElement);
+}
+
+//checkerboard
+let checkerboard = [
+  null, 0, null, 1, null, 2, null, 3,
+  4, null, 5, null, 6, null, 7, null,
+  null, 8, null, 9, null, 10, null, 11,
+  null, null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null,
+  12, null, 13, null, 14, null, 15, null,
+  null, 16, null, 17, null, 18, null, 19,
+  20, null, 21, null, 22, null, 23, null 
+]
+
+//index.html
+const squares = $("td");
+let silverCoins = $(".silverCoin");
+let goldCoins = $("span");
+const silverTurnText = $(".silverTurnText");
+const goldTurnText = $(".goldTurnText");
+const divider = $("#divider");
+
+//properties
+let turn = true;
+let silverScore = 12;
+let goldScore = 12;
+let userCoins;
+
+//create an object
+let chosenCoin = {
+  coinId: -1,
+  indexOfCoin: -1,
+  isCaptain: false,
+//movement on board back and forth
+  sevenSq: false,
+  nineSq: false,
+  fourteenthSq: false,
+  eighteenthSq: false,
+  negSevenSq: false,
+  negNineSq: false,
+  negFourteenthSq: false,
+  negEighteenthSq: false,
+}
+
+//event listeners on coins
+function clickOnCoins() {
+  if (turn) {
+    for (let i = 0; i < silverCoins.length; i++) {
+      silverCoins[i].on('click', getUserCoins);
+    }
+  } else {
+     for (let i = 0; i < goldCoins.length; i++) {
+      goldCoins[i].on('click', getUserCoins);
+    }
+  }
+  console.log("work");
+}
+
+
+
+
+//function for length of the players coin count
+function getUserCoins() {
+  if (turn) {
+    userCoins = silverCoins;
+  } else {
+    userCoins = goldCoins;
+  }
+  // removeSquareonclick();
+  // resetBorders();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let silverCoin = document.createElement("img");
 // silverCoin.setAttribute("src", "https://i.postimg.cc/Bbb4MR3z/A17-F0-D58-1-C69-405-E-99-E3-91-B03-E8-E7957.png");
@@ -46,70 +171,5 @@ $("#exitGame").on('click', function() {
 // goldCoin.setAttribute("alt", "Gold");
 // document.getElementById("goldCoins").appendChild(goldCoin);
 
-//making the checkerboard
-//td=individual data cell/square  tr=row
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//water movement
-
-let h = $('.flag').width();
-for(let i = 0; i < h; i++){
-    let flagElement = $("<div class='flag-element'>");
-    flagElement.css('background-position', -i + "px 0");
-    flagElement.css('-webkit-animation-delay', i * 2 + 'ms');
-    flagElement.css('-moz-animation-delay', i * 2 + 'ms');
-    flagElement.css('-ms-animation-delay', i * 2 + 'ms');
-    flagElement.css('animation-delay', i * 2 + 'ms');
-    $('.flag').append(flagElement);
-}
-
-
-
-// document.querySelector("#s-Button_2")//join/newsletter button
-//   document.querySelector("#s-Rectangle_1")//whole newsletter drop-in
-//     document.querySelector("#s-Input_1 > div.borderLayer > div > div > div > input[type=text]")//text-email-input for newsletter
-//     document.querySelector("#s-Rectangle_2 > div.borderLayer > div > div")//subscribe button
-
-// document.querySelector("#rtr-s-Button_1_0")//rules/directions button
-//   document.querySelector("#s-Paragraph_3")//directions and rules paragraph
-
-// document.querySelector("#s-Rectangle_1")//Ready to play button
-
-// document.querySelector("#s-Button_4")//reset button
-
-// document.querySelector("#s-Button_5")//exit game button
-
-
-
-// //hidden elements on page load event
-
-// document.querySelector("#s-Paragraph_3")//rules and directions hidden on pageload
-
-// document.querySelector("#s-Rectangle_1")//subscribe to newsletter 
-
-// document.querySelector("#s-Button_4")//reset gameboard button
-
-// document.querySelector("#s-Button_5")//exit game button
-
-// document.querySelector("#s-Image_1")//gameboard 
-
-// document.querySelector("#s-Image_24")
 
